eecf2f3b4f86d02669d76e0ba801c6fab51f9335
govind <singhgovind770@gmail.com>
2022-09-14 01:29:09 +0530

    Upaded...

--- model/driverModel.js
+++ model/driverModel.js
@@ -16,11 +16,9 @@ const DriverSchema = new mongoose.Schema({
         type: {
           type: String, // Don't do `{ location: { type: String } }`
           enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
         },
         coordinates: {
-          type: [Number],
-          required: true
+          type: [Number]
         }
       },
     
